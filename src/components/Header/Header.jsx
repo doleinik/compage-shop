a47bb494dc5e15383cb9f668f216ff819e673b25
Doleinik <doleinika@gmail.com>
2022-10-23 00:18:39 +0300

    init

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
         <div className={'header'}>
             <Button onClick={onClose}>Закрыть</Button>
             <span className={'username'}>
-                {user?.username}
+                Hello, <span>{user?.username}</span>
             </span>
         </div>
     );
