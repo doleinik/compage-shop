be0965d54df386f34894ca83a2aab3b4d4ce1ff7
Doleinik <doleinika@gmail.com>
2022-10-22 15:00:46 +0300

    init

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const {tg, user, onClose} = useTelegram()
     return (
         <div className={'header'}>
             <Button onClick={onClose}>Close</Button>
-            <span className={'username'}>{user?.username}</span>
+            <span className={'username'}>{user}</span>
         </div>
     );
 };
